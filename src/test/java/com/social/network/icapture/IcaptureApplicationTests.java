92bca2c6c77e591f066f88b21c689a5f038fd4bc
SherryXiaoyingLi <xl2gs@virginia.edu>
2021-05-25 01:32:53 -0700

    application properties & Spring boot test initial

---
+++ src/test/java/com/social/network/icapture/IcaptureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.network.icapture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcaptureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
