c44356878d8bee42b563acbfdc71762a0c7251b4
SherryXiaoyingLi <xl2gs@virginia.edu>
2021-05-26 23:31:57 -0700

    initial set up

---
+++ client/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css';
+
+// class Welcome extends React.Component {
+//     render() {
+//         return (
+//             <div>Welcome! {this.props.name}</div>
+//         );
+//     }
+// }
+
+// function User(props) {
+//     return (
+//         <div>Is your username {props.name}?</div>
+//     );
+// }
+
+// ========================================
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
